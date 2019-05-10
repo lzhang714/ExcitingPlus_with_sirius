8517495544597d73e243f7a0af3d1a013344052f
lzhang714 <longzhang2008@gmail.com>
2019-05-10 04:21:24 -0400

    changed to use modmain only, and better comments

--- src/gengvec.f90
+++ src/gengvec.f90
@@ -1,8 +1,6 @@
-
 ! Copyright (C) 2002-2005 J. K. Dewhurst, S. Sharma and C. Ambrosch-Draxl.
 ! This file is distributed under the terms of the GNU General Public License.
 ! See the file COPYING for license details.
-
 !BOP
 ! !ROUTINE: gengvec
 ! !INTERFACE:
@@ -10,9 +8,9 @@ subroutine gengvec
 ! !USES:
   use modmain
   use modtest
-#ifdef _SIRIUS_
-  use mod_sirius
-#endif
+!#ifdef _SIRIUS_
+!  use mod_sirius
+!#endif
 ! !DESCRIPTION:
 !   Generates a set of ${\bf G}$-vectors used for the Fourier transform of the
 !   charge density and potential and sorts them according to length. Integers
@@ -39,38 +37,60 @@ subroutine gengvec
   integer, allocatable :: iar(:)
   real(8), allocatable :: rar(:)
 
-  ! LZ ----------------------------------------------------------------------------
-  ! LZ: a big if-else to relocate the original code in between "else" and "end if"
-
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   if (use_sirius_library.and.use_sirius_gvec) then
+  
 #ifdef _SIRIUS_
-    ! allocate several global G-vector arrays
-    ! ngvec: number of G-vec
-    ngvec = sirius_get_num_gvec(sctx)    
-    ! ivg: G-vec in integer(lattice) coordi
-    If (allocated(ivg)) deallocate (ivg) 
+
+    ! ---------------------------------------------------------------
+    ! remind myself some EP definitions: 
+    !   ivg:   G-vec in integer(lattice) coordi
+    !   intgv: integer grid intervals for each direction, integer(3,2)
+    !   ivgig: map from integer grid to G-vector array
+    !   igfft: map from G-vector array to FFT array
+    !   vgc:   G-vec in Cartesian coordi
+    !   gc:    length of G-vectors, size=ngvec
+    ! ---------------------------------------------------------------
+    
+    ! 1st, get from sirius: ngvec
+    ! 
+    ! Note: sirius_get_num_gvec(sctx) will return int sim_ctx.gvec().num_gvec(), 
+    !       Sirius documentation says it is "total number of G vectors", but don't confuse it with "ngrtot".
+    !       This gives us ngvec rather than ngrtot. 
+    ! 
+    ngvec = sirius_get_num_gvec(sctx)
+    ! 
+    ! 2nd, allocate several arries in EP side, using the "ngvec"
+    ! 
+    If (allocated(ivg)) deallocate (ivg)
     Allocate(ivg(3,ngvec))
-    ! intgv: integer grid intervals for each direction, integer(3,2)
-    ! ivgig: map from integer grid to G-vector array
     If (allocated(ivgig)) deallocate (ivgig)
-    Allocate(ivgig(intgv(1,1):intgv(1,2),intgv(2,1):intgv(2,2),intgv(3,1):intgv(3, 2)))  
-    ! igfft: map from G-vector array to FFT array
+    Allocate(ivgig(intgv(1,1):intgv(1,2),intgv(2,1):intgv(2,2),intgv(3,1):intgv(3, 2)))
     If (allocated(igfft)) deallocate (igfft)
     Allocate(igfft(ngvec))
-    ! vgc: G-vec in Cartesian coordi
-    If (allocated(vgc)) deallocate (vgc) 
+    If (allocated(vgc)) deallocate (vgc)
     Allocate(vgc(3,ngvec))
-    ! gc: length of G-vectors
     If (allocated(gc)) deallocate (gc)
     Allocate(gc(ngvec))
-    ! pass G-vec array to sirius
+    !
+    ! 3rd, get from sirius: several G-vec arraies;
+    !      get from sirius: the mapping between G-vec index and FFT index;
+    ! 
     call sirius_get_gvec_arrays(sctx, ivg(1,1), vgc(1,1), gc(1), ivgig(intgv(1,1), intgv(2,1), intgv(3,1)))
-    ! pass the mapping between G-vec index and FFT index to sirius
     call sirius_get_fft_index(sctx, igfft(1))
+
+              write(*,*)'  '    
+              write(*,*)' debug flag, gengvec, 1 '
+              write(*,'(" debug flag, gengvec, ngrtot(from EP)     = ", I10    )') ngrtot
+              write(*,'(" debug flag, gengvec, ngvec(from SIRIUS)  = ", I10    )') ngvec
+              write(*,*)'  ' 
+    
 #else
     stop sirius_error
 #endif
+
   else
+
     ! allocate local arrays
     allocate(idx(ngrtot))
     allocate(iar(ngrtot))
@@ -86,7 +106,7 @@ subroutine gengvec
     allocate(vgc(3,ngrtot))
     if (allocated(gc)) deallocate(gc)
     allocate(gc(ngrtot))
-
+    ! find length of each G-vector
     ig=0
     do i1=intgv(1,1),intgv(1,2)
       do i2=intgv(2,1),intgv(2,2)
@@ -103,27 +123,25 @@ subroutine gengvec
         end do
       end do
     end do
-
     ! sort by vector length
     call sortidx(ngrtot,gc,idx)
-
     ! re-order arrays
     do ig=1,ngrtot
-      rar(ig)=gc(ig)
+      rar(ig)=gc(ig)            ! copy gc to rar
     end do
-    do ig=1,ngrtot
-      gc(ig)=rar(idx(ig))
+    do ig=1,ngrtot 
+      gc(ig)=rar(idx(ig))       ! re-assign rar to gc
     end do
     do k=1,3
       do ig=1,ngrtot
-        iar(ig) = ivg(k,ig)
+        iar(ig) = ivg(k,ig)       ! copy ivg to iar
       end do
       do ig=1,ngrtot
-        ivg(k,ig) = iar(idx(ig))
+        ivg(k,ig) = iar(idx(ig))  ! re-assign iar to ivg
       end do
     end do
     ivgig(:,:,:) = 0
-    do ig=1,ngrtot
+    do ig=1,ngrtot         ! loop ig, this gives ivgig,vgc,igfft, from sorted ivg
       i1 = ivg(1,ig)
       i2 = ivg(2,ig)
       i3 = ivg(3,ig)
@@ -149,7 +167,6 @@ subroutine gengvec
       end if
       igfft(ig)=j3*ngrid(2)*ngrid(1)+j2*ngrid(1)+j1+1
     end do ! loop ig
-
     ! find the number of vectors with G < gmaxvr
     ngvec=1
     do ig=ngrtot,1,-1
@@ -159,15 +176,20 @@ subroutine gengvec
       end if
     end do
 10  continue
-
     ! write number of G-vectors to test file
     call writetest(900,'number of G-vectors',iv=ngvec)
-
+    ! 
     deallocate(idx,iar,rar)
 
+
+              write(*,*)'  '    
+              write(*,*)' debug flag, gengvec, 2 '
+              write(*,'(" debug flag, gengvec, ngrtot = ", I10    )') ngrtot
+              write(*,'(" debug flag, gengvec, ngvec  = ", I10    )') ngvec
+              write(*,*)'  ' 
+
   end if
-  ! LZ: a big if-else to relocate the original code in between "else" and "end if"
-  ! LZ ----------------------------------------------------------------------------
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
   return
 
